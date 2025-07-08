c7f1733cd26394364b95350e8799928a1c699c8a
DevDias18 <geovannifvl18@gmail.com>
2025-07-08 15:33:39 -0300

    Aprendendo coleções = Lista

---
+++ lista.py
@@ -0,0 +1,26 @@
+
+
+# Vamos começar com uma lista de números inteiros.
+lista = [1,2,3,4,5,6,7,8,9,10]
+
+# lista de strings.
+lista_de_string = [ "um", "dois" , "três"]
+
+# lista de listas.
+lista_de_listas = [[1,2,3], [4,5,6],[7,8,9]]
+
+# lista de dicionário.
+lista_de_dicionarios = [{"nome": "João", "idade": 30}, {"nome": "Maria", "idade": 25}]
+
+# lista de tuplas.
+lista_de_tuplas = [(1,2,3), (4,5,6), (7,8,9)]
+
+# lista vasia.
+lista_vasia = []
+
+# cria uma lista com 11 inteiros de 0 a 10.
+lista1 = list(range(11))
+
+# Cria uma lista com todas as letras do nome, incluindo o espaço.
+lista2 = list('Geovanni Moreira')
+
